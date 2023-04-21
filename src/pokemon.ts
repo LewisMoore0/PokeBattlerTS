e513fed1a03f45b808d94e0e1c63276e89c1f8f9
Lewis Moore <lewismooredev@gmail.com>
2023-04-21 19:49:53 +0100

    Add attack and has fainted functions

--- src/pokemon.ts
+++ src/pokemon.ts
@@ -11,8 +11,16 @@ export default class Pokemon {
         this.move = 'tackle'
     }
 
-    takeDamage(damage: number){
+    takeDamage(damage: number): string{
         this.health -= damage
         return `${this.name} took ${damage} damage!`
     }
-}
+
+    attack(): number {
+        return this.attackDamage
+    }
+
+    hasFainted(): boolean {
+        return this.health <= 0 ? true : false
+    }
+} 
--- test/pokemon.test.js
+++ test/pokemon.test.js
@@ -26,4 +26,23 @@ describe('Pokemon', () => {
         expect(pokemon.takeDamage(10)).toEqual('Lewis took 10 damage!')
         expect(pokemon.health).toBe(40)
     })
+
+    test('attack function returns the pokemons attackDamage', () => {
+        const pokemon = new Pokemon('Lewis', 50, 10)
+        expect(pokemon.attack()).toEqual(10)
+    })
+
+
+    test('hasFainted function returns true if pokemons HP is zero', () => {
+        const pokemon = new Pokemon('Lewis', 50, 10)
+        pokemon.takeDamage(50)
+        expect(pokemon.hasFainted()).toEqual(true)
+    })
+
+
+    test('hasFainted function returns false if pokemons HP is not zero', () => {
+        const pokemon = new Pokemon('Lewis', 50, 10)
+        pokemon.takeDamage(40)
+        expect(pokemon.hasFainted()).toEqual(false)
+    })
 })
