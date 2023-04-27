4d1917e88429bb58d02e9fc6e12a6679acd8b3c2
Lewis Moore <lewismooredev@gmail.com>
2023-04-27 09:22:57 +0100

    Add weak and effective functions to grassPokemon

--- src/grassPokemon.ts
+++ src/grassPokemon.ts
@@ -1,4 +1,6 @@
+import FirePokemon from "./firePokemon";
 import Pokemon from "./pokemon";
+import WaterPokemon from "./waterPokemon";
 
 export default class GrassPokemon extends Pokemon {
     type: string
@@ -6,5 +8,13 @@ export default class GrassPokemon extends Pokemon {
     constructor(name: string, health: number, attackDamage: number) {
         super(name, health, attackDamage)
         this.type ='grass'
+    }
+
+    isEffectiveAgainst(pokemon: FirePokemon | GrassPokemon | WaterPokemon): boolean {
+        return pokemon.type === 'water' ? true : false
+    }
+
+    isWeakTo(pokemon: FirePokemon | GrassPokemon | WaterPokemon): boolean {
+        return pokemon.type === 'fire' ? true : false
     }
 }
--- test/grassPokemon.test.js
+++ test/grassPokemon.test.js
@@ -1,5 +1,7 @@
+import FirePokemon from '../src/firePokemon'
 import GrassPokemon from '../src/grassPokemon'
 import Pokemon from '../src/pokemon'
+import WaterPokemon from '../src/waterPokemon'
 
 describe('GrassPokemon', () => {
     test('GrassPokemon shoudl extend pokemon class', () => {
@@ -10,5 +12,19 @@ describe('GrassPokemon', () => {
     test('waterPokemon should have type prop of fire', () => {
         const grassPokemon = new GrassPokemon('Lewis', 100, 25)
         expect(grassPokemon.type).toEqual('grass')
+    })
+
+    test('isEffectiveAgainst method returns true when given water pokemon', () => {
+        const waterPokemon = new WaterPokemon('Lewis', 100, 25)
+        const grassPokemon = new GrassPokemon('Lewis', 100, 25)
+
+        expect(grassPokemon.isEffectiveAgainst(waterPokemon)).toEqual(true)
+    })
+
+    test('isWeakTo method returns true when given fire pokemon', () => {
+        const firePokemon = new FirePokemon('Lewis', 100, 25)
+        const grassPokemon = new GrassPokemon('Lewis', 100, 25)
+
+        expect(grassPokemon.isWeakTo(firePokemon)).toEqual(true)
     })
 })
