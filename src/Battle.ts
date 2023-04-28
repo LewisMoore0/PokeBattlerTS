6e8404156174ade605916ac071f6135d785ba794
Lewis Moore <lewismooredev@gmail.com>
2023-04-28 16:16:11 +0100

    Add fight function with turn based attacking

--- src/Battle.ts
+++ src/Battle.ts
@@ -1,4 +1,7 @@
+import FirePokemon from "./firePokemon";
+import GrassPokemon from "./grassPokemon";
 import Trainer from "./Trainer";
+import WaterPokemon from "./waterPokemon";
 
 export default class Battle {
     trainer1: Trainer
@@ -13,5 +16,26 @@ export default class Battle {
 
     switchTurns(){
         return this.isTrainer1Turn === true ? this.isTrainer1Turn = false : this.isTrainer1Turn = true
+    }
+
+    fight(pokemon1: FirePokemon | WaterPokemon | GrassPokemon, pokemon2: FirePokemon | WaterPokemon | GrassPokemon){
+        if (this.isTrainer1Turn){
+            if (pokemon1.isEffectiveAgainst(pokemon2)){
+                pokemon2.takeDamage(pokemon1.attack() * 1.25)
+            }
+            if (pokemon1.isWeakTo(pokemon2)){
+                pokemon2.takeDamage(pokemon1.attack() * 0.75)
+            }
+        }
+
+        if(!this.isTrainer1Turn){
+            if (pokemon2.isEffectiveAgainst(pokemon1)){
+                pokemon1.takeDamage(pokemon2.attack() * 1.25)
+            }
+            if (pokemon2.isWeakTo(pokemon1)){
+                pokemon1.takeDamage(pokemon2.attack() * 0.75)
+            }
+        }
+        this.switchTurns()
     }
 }
--- test/Battle.test.js
+++ test/Battle.test.js
@@ -1,5 +1,7 @@
 import Battle from '../src/Battle'
 import Trainer from '../src/Trainer'
+import Charmander from '../src/Charmander'
+import Squirtle from '../src/Squirtle'
 
 describe('Battle', () => {
     test('takes two Trainer objects', () => {
@@ -25,5 +27,19 @@ describe('Battle', () => {
 
         battle.switchTurns()
         expect(battle.isTrainer1Turn).toEqual(false)
+    })
+
+    test('fight function takes two pokemon, and Trainer1 pokemon attacks first and tkaes in to account strengths and weaknesses', () => {
+        const trainer1 = new Trainer('Lewis')
+        const trainer2 = new Trainer('Anna')
+        const pokemon1 = new Charmander('charmander', 100, 10)
+        const pokemon2 = new Squirtle('squirtle', 100, 10)
+        const battle = new Battle(trainer1, trainer2)
+
+        battle.fight(pokemon1, pokemon2)
+        expect(pokemon2.health).toBe(92.5)
+
+        battle.fight(pokemon1, pokemon2)
+        expect(pokemon1.health).toBe(87.5)
     })
 })
